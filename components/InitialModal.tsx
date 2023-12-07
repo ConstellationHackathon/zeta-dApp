d5bac9c846b62c0d8404f9405e6b92496dbf7192
Sebas <sheva3232@gmail.com>
2023-12-07 16:47:55 -0500

    working on second Modal and polishing details

--- components/InitialModal.tsx
+++ components/InitialModal.tsx
@@ -1,7 +1,11 @@
 import { IconClose } from './Icon'
 import Modal from 'react-modal';
-
-const InitialModal: React.FC<{ closeModal: () => void }> =  ({ closeModal }) => {    
+interface ResponseModalProps {
+    closeModal: () => void;
+    onDoneButton: () => void;
+}
+Modal.setAppElement('#__next');
+const InitialModal: React.FC<ResponseModalProps> =  ({ closeModal, onDoneButton }) => {    
     return (
         <Modal
             isOpen={true}
@@ -9,6 +13,7 @@ const InitialModal: React.FC<{ closeModal: () => void }> =  ({ closeModal }) => 
             contentLabel="Example Modal"
             className="fixed inset-0 flex flex-col gap-[48px] p-[24px] rounded-[32px] shadow-lg m-auto w-[700px] h-[600px] bg-[#fafafa] text-black z-10 text-2xl justify-center"
             overlayClassName="fixed inset-0 bg-black bg-opacity-20"
+            appElement={document.getElementById('root') as HTMLElement}
         >
             <div className='flex flex-col w-full gap-1 items-center'>
                 <div>Send 0.055 BTC to the address</div>
@@ -25,6 +30,7 @@ const InitialModal: React.FC<{ closeModal: () => void }> =  ({ closeModal }) => 
             </div>
             <button
                 className='bg-black text-white rounded-full px-[32px] py-[8px] font-semibold w-fit mx-auto'
+                onClick={() => onDoneButton()}
             >
                 Done
             </button>
