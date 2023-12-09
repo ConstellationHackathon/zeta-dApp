6826858a95de46fc2a6a08bc5306c8838c66fb41
Sebas <sheva3232@gmail.com>
2023-12-09 14:35:41 -0500

    modal check working

--- graphql/FetchSenderInfo.tsx
+++ graphql/FetchSenderInfo.tsx
@@ -1,14 +1,21 @@
 import { gql } from "@apollo/client";
 
 export const AvaxSenderEvents = gql`
-    query AvaxSenderEvents {
-      avaxReceiveds {
-        transactionId
-        transactionHash
-        tokenAmount
-        sender
-        id
-        timestamp
-      }
+  query AvaxSenderEvents {
+    avaxReceiveds {
+      transactionId
+      transactionHash
+      tokenAmount
+      sender
+      id
+      timestamp
     }
+    amountSentInUSDs {
+      sender
+      id
+      amountSent
+      timestamp
+      transactionHash
+    }
+  }
 `;
